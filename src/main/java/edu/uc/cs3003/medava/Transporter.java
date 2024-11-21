3dac7b3a584f858bf2cd7e0625cfca38d37c29c2
heathnate <nateheath04@gmail.com>
2024-11-21 15:03:42 -0500

    Checkpoint 2

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -4,17 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Transporter {
+    private String mTransporterName;
+    private List<Medicine> goods;
+    private double mLowTemperature, mHighTemperature;
+
     // Instance initializer
     {
         goods = new ArrayList<Medicine>();
     }
 
-    private String mTransporterName;
-    public List<Medicine> goods;
-    
+    public Transporter(String transporterName, double lowTemp, double highTemp) {
+        mTransporterName = transporterName;
+        mLowTemperature = lowTemp;
+        mHighTemperature = highTemp;
+    }
 
     public String getTransporterName() {
         return mTransporterName;
+    }
+
+    public boolean load(Medicine itemToLoad) {
+        if (itemToLoad.isTemperatureRangeAcceptable(mLowTemperature, mHighTemperature)) {
+            System.out.println(String.format("Adding a %s to the transporter.", itemToLoad.getMedicineName()));
+            goods.add(itemToLoad);
+            return true;
+        }
+        return false;
+    }
+
+    public Medicine unload() {
+        return goods.remove(0);
+    }
+    public boolean isEmpty() {
+        return goods.isEmpty();
     }
 
     public void ship() {
