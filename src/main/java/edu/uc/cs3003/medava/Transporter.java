2d94fd76db350e63f782e0427ecc0271766a90da
heathnate <nateheath04@gmail.com>
2024-11-21 14:54:34 -0500

    Transporter class skeleton

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,23 @@
 package edu.uc.cs3003.medava;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Transporter {
+    // Instance initializer
+    {
+        goods = new ArrayList<Medicine>();
+    }
+
+    private String mTransporterName;
+    public List<Medicine> goods;
+    
+
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+
+    public void ship() {
+        // Do some shipping!
+      }
 }
