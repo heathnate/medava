c98f8aadcefbf2da411552e11ee924c3354cb12e
heathnate <nateheath04@gmail.com>
2024-11-21 14:58:18 -0500

    HospitalRunner class implementation

--- src/main/java/edu/uc/cs3003/medava/SupplyChain.java
+++ src/main/java/edu/uc/cs3003/medava/SupplyChain.java
@@ -3,5 +3,6 @@ package edu.uc.cs3003.medava;
 public class SupplyChain {
     public static void main(String args[]) {
         // System.out.println("Hello, Nate! This is the WOPR speaking.");
+        HospitalRunner.run();
     }
 }
